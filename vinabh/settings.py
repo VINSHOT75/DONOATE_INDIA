b09e1d02baea04cbdde9567a1660d92450cdcdb0
Vinayak Singh Bassan <83154092+VINSHOT75@users.noreply.github.com>
2021-04-30 17:20:16 +0530

    Update settings.py

--- vinabh/settings.py
+++ vinabh/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKENDS= 'django.core.mail.backends.smtp.EmailBackends'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vinshot75@gmail.com'
-EMAIL_HOST_PASSWORD = 'Temporary@'
+EMAIL_HOST_PASSWORD = 
 EMAIL_USE_TLS = True 
